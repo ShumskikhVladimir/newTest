d1b6ddd1d66351abb619b7d0d85b2609a3250e87
Shumskikh Vladimir <shumskikhv@gmail.com>
2024-04-06 12:59:27 +0500

    Third commit deleted lines

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,6 @@ public class Main {
         bigCat.setName("Pushok");
         System.out.println("The name of big cat is " + bigCat.getName());
         System.out.println("The name of big cat is " + bigCat.getName());
-        System.out.println("The name of big cat is " + bigCat.getName());
-        System.out.println("The name of big cat is " + bigCat.getName());
-        System.out.println("The name of big cat is " + bigCat.getName());
-
         System.out.println("The name of small cat is " + smallCat.getName());
     }
 }
